f83645fbb6c207c33fb358a35db9b56bb44188e4
NicholasNeto <nicholasneto@outlook.com>
2019-02-15 16:35:57 -0200

    utilizando state em componentes React

--- src/js/navbar/nav-menu.jsx
+++ src/js/navbar/nav-menu.jsx
@@ -3,11 +3,23 @@ import React from 'react';
 
 class NavMenu extends React.Component {
 
+  constructor(props){
+    super(props)
+    this.state = {menuActive: "Home"}
+    this.alteraActive = this.alteraActive.bind(this)
+  }
+
+  alteraActive(titulo, self){
+    self.setState({menuActive: titulo})
+  }
+
   render(){
+
+    let self = this
 
     let listOfMenu = this.props.menu.map(function(value) {
       return (
-        <li key={value.titulo}> <a href={value.link} > {value.titulo} </a> </li>
+        <li key={value.titulo} onClick={self.alteraActive.bind(null,value.titulo,self)} className={self.state.menuActive == value.titulo ? "active": ""} > <a href={value.link} > {value.titulo} </a> </li>
       )
     });
 

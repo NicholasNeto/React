7d066a3f22204dac9d0058ceb0408f4dc53b41d4
NicholasNeto <nicholasneto@outlook.com>
2019-02-19 07:40:46 -0300

    implementando Date & Time

--- src/js/titulo/titulo.jsx
+++ src/js/titulo/titulo.jsx
@@ -2,8 +2,31 @@ import React from "react";
 
 
 class Titulo extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {hora: new Date()}
+  }
+
+
+  componentDidMount(){
+    this.horaID = setInterval(
+      ()=> this.atualizarSegundos(),
+      1000
+    );
+  }
+
+  atualizarSegundos(){
+    this.setState({hora: new Date()});
+  }
+
+  componentWillUnMount(){
+    clearInterval(this.horaID)
+  }
+
+
   render(){
-    return <h1>Iniciando com React</h1>;
+    return <h1>Iniciando com React {this.state.hora.toLocaleTimeString()}</h1>;
   }
 }
 

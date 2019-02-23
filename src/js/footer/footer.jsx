524570aaf467bfa254930a31e4b0a8f197b0c7c5
NicholasNeto <nicholasneto@outlook.com>
2019-02-23 15:38:36 -0300

    trabalhando com o ciclo de vida de componentes

--- src/js/footer/footer.jsx
+++ src/js/footer/footer.jsx
@@ -2,6 +2,27 @@ import React from "react"
 
 class Footer extends React.Component {
 
+  constructor(props){
+    super(props)
+
+    this.state = {hora: new Date()}
+  }
+
+  componentDidMount(){
+    this.horaID = setInterval(
+      ()=> this.atualizarSegundos(),
+      1000
+    );
+  }
+
+  atualizarSegundos(){
+    this.setState({hora: new Date()});
+  }
+
+  componentWillUnMount(){
+    clearInterval(this.horaID)
+  }
+
   render(){
 
     return(
@@ -12,6 +33,7 @@ class Footer extends React.Component {
               <div className="col l6 s12">
                 <h5 className="white-text">Footer Content</h5>
                 <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                <p className="grey-text text-lighten-4">Teste foi publcicado as: {this.state.hora.toLocaleTimeString()}</p>
               </div>
               <div className="col l4 offset-l2 s12">
                 <h5 className="white-text">Links</h5>
